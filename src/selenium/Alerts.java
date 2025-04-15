f3ebd2b335355335c277e21b6c53c67a270e10d8
KUSUMA ABHINAV <KUSUMA ABHINAV@LAPTOP-151VN14N>
2025-04-15 12:00:35 +0530

    second commit

--- src/selenium/Alerts.java
+++ src/selenium/Alerts.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Alerts {
-
+//abinav
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
